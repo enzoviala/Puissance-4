b5e632645459e59b53cd3b0cccdbb9a2bf897c59
Ravitoto301 <herimanana.rasolonirina@eleve.egd.mg>
2020-02-12 10:20:59 +0100

    fonction victoire et match nul

--- Puissance-4.py
+++ Puissance-4.py
@@ -23,6 +23,7 @@ def affiche(grille):
                 print('|O', end='')
         print("|")
 
+
 def coup_possible(grille, col):
         if grille[0][col]==1:
             return False
@@ -65,7 +66,7 @@ assert jouer([[0, 0, 0, 0, 0, 0, 0],
  [0, 0, 2, 0, 0, 0, 0],
  [0, 0, 1, 0, 0, 0, 0]]
 
-def horiz (gril,j,lig):
+def horiz (gril, j, lig):
     tabl=gril
     m=0
     if tabl[lig][0]==j and tabl[lig][1]==j and tabl[lig][2]==j and tabl[lig][3]==j:
@@ -98,8 +99,10 @@ assert horiz([[0, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0],
  [2, 2, 2, 2, 0, 0, 0]],2,5)==True
 
-def vert (gril,j,lig,col):
+def vert (gril, j, lig, col):
     tabl=gril
+    if lig>2:
+        return False
     if tabl[lig][col]==j and tabl[lig+1][col]==j and tabl[lig+2][col]==j and tabl[lig+3][col]==j:
         return True
     else :
@@ -174,9 +177,10 @@ assert diag_haut([[1, 0, 0, 0, 0, 0, 0],
  [0, 0, 1, 0, 0, 0, 0],
  [0, 0, 0, 1, 0, 0, 0]],1,5,3)==True
 
+
 def diag_bas(grille, j, lig, col):
     n=1
-    if lig>3:
+    if lig>2:
         return False
     if col>3:
         if grille[lig+1][col-1]==j:
@@ -230,4 +234,42 @@ assert diag_bas([[1, 0, 0, 0, 0, 0, 0],
  [0, 1, 0, 0, 0, 0, 0],
  [0, 0, 1, 0, 0, 0, 0],
  [0, 0, 0, 1, 0, 0, 0]],1,2,0)==True
-            
+  
+def victoire (gril, j, lig, col):
+    if horiz(gril, j, lig)==True or vert (gril, j, lig, col)==True or diag_haut(gril,j,lig,col)==True or diag_bas(gril,j,lig,col)==True:
+        return True 
+    else: 
+        return False
+assert victoire([[0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0],
+ [1, 0, 0, 1, 0, 0, 0],
+ [0, 1, 0, 1, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0]],1,2,3)==True
+assert victoire([[0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0],
+ [1, 0, 0, 1, 0, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0]],1,2,3)==False
+
+def match_nul(gril,j, col, lig):
+    for k in range(1,3):
+        for l in range(7):
+            for z in range(6):
+                if victoire(gril, k , z, l)==False: 
+                    if coup_possible(gril, l)==False:
+                        return True
+    return False
+assert match_nul([[1, 1, 2, 2, 1, 2, 1],
+ [1, 1, 2, 1, 2, 2, 1],
+ [2, 1, 2, 2, 1, 2, 2],
+ [2, 2, 1, 2, 2, 1, 1],
+ [2, 1, 1, 2, 2, 1, 2],
+ [1, 2, 1, 1, 2, 1, 1]],1,1,1)==True
+assert match_nul([[0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0],
+ [1, 0, 0, 0, 0, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0]],1,1,1)==False
